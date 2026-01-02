ca237b155d4ba3ee5e67c2fdd8f4900beac7df8f
Brandon <bbunnyeng0930@gmail.com>
2026-01-02 09:24:16 -0500

    #Lesson 1

--- Beginner/hello.c
+++ Beginner/hello.c
@@ -1,4 +1,12 @@
 # include <stdio.h>
 int main(void){
+    int i;
+    float f;
+    i = 22;
+    f = 3.14;
+    char *s = "Hello, world!";
     printf("Hello, World!\n");
+    printf("i = %d\n", i);
+    printf("f = %f\n", f);
+    printf("s = %s\n", s);
 }
